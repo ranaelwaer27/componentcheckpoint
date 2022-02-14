e620afcf90f46830a368c5da775388ccda7ddfa7
ranaelwaer27 <ranaelouaer12@gmail.com>
2022-02-14 16:20:47 +0100

    first commit

---
+++ component/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import img from './pdp.jpg';
+
+function App() {
+  return (
+    <div >
+      <h1 class="title red">Rana</h1>
+      <h1 class="title red">Zaghouan</h1>
+      <img src={img}/>
+      <img src="./Pro.jpg" />
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ component/src/FullName.js
@@ -0,0 +1,6 @@
+
+
+function Welcome(props) {
+    return <h1>Bonjour, {props.name}</h1>;
+  }
+  export default Welcome;
